ff96b0c7735ccdc956b26e2d203408e6d09eb8bf
villetan <ville.tanskanen@helsinki.fi>
2016-01-15 15:21:50 +0200

    tehtava5 done

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+def puuttuva(taulukko)
+  taulukko=taulukko.sort_by(&:to_i)
+
+  for i in 0..taulukko.last
+      return i if !taulukko.include? i
+  end
+  palautettava = taulukko.last.next
+  return palautettava
+end
