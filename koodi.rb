ab07a49567a8f3fcfa0000064258b38d1126a188
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-01-16 03:41:54 +0200

    tehtävä 5

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,11 @@
-# kirjoita koodi tänne
+def puuttuva (taulu)
+  pituus = taulu.size
+  summa = 0
+  for i in 0..pituus
+    summa = summa + i
+  end
+  taulu.each do |a|
+    summa = summa - a
+  end
+  prints summa
+end
