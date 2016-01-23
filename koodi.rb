96df518a6fe9d3c217ac193b829958646dff5621
henri <pulperi@gmail.com>
2016-01-23 19:00:38 +0200

    palautus

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,17 @@
 # kirjoita koodi tänne
+def puuttuva(array)
+  luku = -1
+  array.sort!
+  array.length.times do |i|
+    if array.at(i) != i
+      luku = i
+      break
+    end
+  end
+  if luku < 0
+    luku = array.length
+  end
+  luku
+end
+
+puuttuva([1,2,3,0])
