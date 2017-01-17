2bedf9c50816a44c56c13d7e60a79c856f827592
JohannaUha <Uha@Johannas-MacBook-Pro.local>
2017-01-17 18:08:39 +0200

    teht5

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
-# kirjoita koodi tänne
+def puuttuva(taulukko)
+  n = taulukko.length
+  puuttuva = ""
+  for i in 0..n
+    if !taulukko.include? i
+      puuttuva = i
+    end
+  end
+  puuttuva
+end
+
+
